0f9413d5e2d2536a7502bb3cb58aa647918b9ec8
scroll <ymarumar502@gmail.com>
2023-08-20 15:02:47 +0500

    first

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = prompt("Введите свое имя: ")
+var age = prompt("Сколько вам лет? ")
+var example1 = prompt ("Решите пример 6 + 3 = ?")
+var example2 = prompt ("Решите пример 20 - 5 = ?")
+var example3 = prompt ("Решите пример 15 * 2 = ?")
+var example4 = prompt ("Решите пример 8 / 4 = ?")
+
+console.log("Меня зовут " + name )
+console.log("Мне " + age )
+console.log("Пример 1: 6 + 3 = 9 Ваш ответ " + example1);
+console.log("Пример 1: 20 - 5 = 15 Ваш ответ " + example2);
+console.log("Пример 1: 15 * 2 = 30 Ваш ответ " + example3);
+console.log("Пример 1: 8 / 4 = 2 Ваш ответ " + example4);
+
+var end = prompt ("Откройте консоль для прсомтра: ")
+
